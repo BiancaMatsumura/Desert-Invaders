97e7fbe756415ee1d503eebda39f2fc19cf8fe6b
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2024-06-26 23:19:00 -0300

    Credits Scene

---
+++ Desert Invaders/Assets/Scripts/creditsController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditsController : MonoBehaviour
+{
+    public UIController controller;
+    public float speed = 50f;
+    public float speedUpgrade = 100f;
+    public bool inputPressed = false;
+    
+    void Start()
+    {
+      
+
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+           inputPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputPressed = true;
+        }
+
+        if (inputPressed) 
+        {
+
+            transform.Translate(Vector2.up * speedUpgrade * Time.deltaTime);
+
+        }
+
+        if (!inputPressed)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if(transform.position.y >= 5500)
+        {
+            controller.Load("menu");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            controller.Load("menu");
+
+        }
+    }
+
+
+}
